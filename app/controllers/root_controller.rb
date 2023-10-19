f300404b3483034391f7c2b4ead2bdaa5db38015
techmoves <muhndlovu28@gmail.com>
2023-10-19 21:55:31 +0200

    cleanup the index modal

--- app/controllers/root_controller.rb
+++ app/controllers/root_controller.rb
@@ -1,8 +1,4 @@
 class RootController < ApplicationController
   def index
-    respond_to do |format|
-      format.html # Render the HTML template
-      format.json { render json: { message: 'JSON response' } }
-    end
   end
 end
